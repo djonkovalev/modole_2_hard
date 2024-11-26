3afef679f276b70e439499cd8eba9dba41073b01
djonkovalev <djon9489@gmail.com>
2024-11-26 17:54:44 +0700

    Add files via upload

---
+++ modole_2_hard.py
@@ -0,0 +1,7 @@
+for elem in range (3, 21):
+    password = f'{elem} - '
+    for x in range(1, elem):
+        for y in range(1, elem):
+            if elem % (x+y) == 0:
+                password += f'{x} + {y} '
+    print(password)
